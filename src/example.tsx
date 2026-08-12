0431cd87cc6840494e59f105024ce3e134673f58
= <saboo9653@gmail.com>
2026-08-12 21:33:52 +0300

    example

--- src/example.tsx
+++ src/example.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+const users = [
+  {
+    id:     1,
+    name:   'Ahmed Mohamed',
+    age:    13,
+    gender: 'male'
+  },
+  {
+    id:     2,
+    name:   'Omnia',
+    age:    13,
+    gender: 'female'
+  },
+]
 
 export default function Example() {
+  const [count, setCount] = useState(0);
+
   return <div>Example</div>;
 }
