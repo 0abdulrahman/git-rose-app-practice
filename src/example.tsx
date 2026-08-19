8f9048d34fb34fe950452add25371e56f0599ff3
ayman taha <lenovo@DESKTOP-KMHVEK4>
2026-08-19 21:39:02 +0300

    Create React Vite app

---
+++ src/example.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const users = [
+  {
+    id:     1,
+    name:   'Ahmed Mohamed',
+    age:    13,
+    gender: 'male'
+  },
+  {
+    id:     2,
+    name:   'Omnia',
+    age:    13,
+    gender: 'female'
+  },
+  {
+    id:     3,
+    name:   'Khaled',
+    age:    21,
+    gender: 'male'
+  }
+]
+
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+  return <div>Example</div>;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const test = "hello";
+
+export const productIds = [1, 2, 3].forEach(p => console.log(p));
+export const pros = [1, 10, 3].forEach(p => console.log(p));
+cd my-app
